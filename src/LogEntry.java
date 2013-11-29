d2713af1d8ea3e5a5424d754ed1bef105bf2eca5
Alex Young <alex@alvdebian>
2013-11-29 20:14:48 +0000

    Added LogEntry setters

--- src/LogEntry.java
+++ src/LogEntry.java
@@ -4,8 +4,33 @@ class LogEntry {
 	private String request;
 	private String response;
 	private String bytesSent;
+	private String referer;
 
 	public LogEntry(String line) {
 
+	}
+	
+	public String getIP() {
+		return ip
+	}
+	
+	public String getTime() {
+		return time;
+	}
+	
+	public String getRequest() {
+		return request;
+	}
+	
+	public String getResponse() {
+		return response;
+	}
+	
+	public String getBytesSent() {
+		return bytesSent;
+	}
+	
+	public String getReferer() {
+		return referer;
 	}
 }
